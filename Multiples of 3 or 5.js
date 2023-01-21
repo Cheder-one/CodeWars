d89426512206ecacea450527d2a4fe2ad14d73ed
Cheder-one <cherbadenis1@gmail.com>
2023-01-21 22:14:25 +0300

    u

--- Multiples of 3 or 5.js
+++ Multiples of 3 or 5.js
@@ -1,5 +1,21 @@
-function solution(number){
+const calcNaturalNumbersBelowNum = (num, ...multiples) => {
+  let divisionResult = [];
   
+
 }
 
-solution(10)
+const numBelow10 = calcNaturalNumbersBelowNum(10, 3, 5)
+console.log(numBelow10);
+
+// Нужно узнать результаты деления чисел до 10-ти при делении их на 3 и 5
+
+// function solution(number){
+
+// }
+
+// console.log(solution(3));
+// console.log(solution(10));
+// console.log(solution(17));
+// console.log(solution(23));
+const arr1 = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = arr1.filter(a => a !== 'e'); // ['a', 'b', 'd', 'f'] 
