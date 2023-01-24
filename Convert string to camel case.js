5faf848c9245118b24c52aa428e09910901746fe
Cheder-one <cherbadenis1@gmail.com>
2023-01-24 22:48:16 +0300

    u

--- Convert string to camel case.js
+++ Convert string to camel case.js
@@ -1,8 +1,14 @@
 function toCamelCase(str){
-
-
-    
+  let words = str.split(/[-_]/);
+  let camelCased = words[0];
+  for (let i = 1; i < words.length; i++) {
+    camelCased += ((words[i][0].toUpperCase() + words[i].substring(1)))
+  }
+  return camelCased;
 }
 
-console.log(toCamelCase("the-stealth-warrior"));
-console.log(toCamelCase("The_Stealth_Warrior"));
+toCamelCase("the-stealth-warrior")
+toCamelCase("The_Stealth_Warrior")
+
+// console.log(toCamelCase("the-stealth-warrior")); // "theStealthWarrior"
+// console.log(toCamelCase("The_Stealth_Warrior")); // "TheStealthWarrior"
