5af2ebe964cea4d7be53b59c0ca676ad6799c56f
Cheder-one <cherbadenis1@gmail.com>
2023-01-26 21:43:58 +0300

    u

--- 04_RomanCalculator.js
+++ 04_RomanCalculator.js
@@ -1,121 +1,81 @@
-https://replit.com/@Cheder-one/RomanCalculator#calculator.js
-https://kataacademy.github.io/kata-program.io/frontend-calculator
-
 function calculator(string) {
   const allowedOperators = ['+', '-', '*', '/'];
   const splittedStr = string.split(/\s+/);
-  const rome = /^(I|II|III|IV|V|VI|VII|VIII|IX|X)$/;
-  const 
-    [I, II, III, IV, V, VI, VII, VIII, IX, X] = 
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
- 
-  // for (const el of splittedStr) {
-  //   if (!allowedOperators.includes(el) && !el.match(/^\d+$/)) {
-  //     console.log(`Invalid operator ${el}`)
-  //     return
-  //   }
-  // };
+  const romanCheck = 
+    /M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})/;
+  const romanNumbers = [''];
 
-  for (const el of splittedStr) {
-    if (el <= 0 || el > 10) {
-      console.log('Операнды должны лежать в диапазоне от 1 до 10');
-      return
+  const createRomanNumbers = () => {
+    const lookup = {
+      M:  1000,
+      CM: 900,
+      D:  500,
+      CD: 400,
+      C:  100,
+      XC: 90,
+      L:  50,
+      XL: 40,
+      X:  10,
+      IX: 9,
+      V:  5,
+      IV: 4,
+      I:  1
+    };
+
+    const generateRomanNumbers = (num) => {
+      result = '';
+      for (const el in lookup) {
+        while (num >= lookup[el]) {
+          result += el;
+          num -= lookup[el]
+        }
+      }
+      return result
     }
-  };
 
-
-
-  
-  const result = Math.floor(eval(string));
-  
-  const convertToRoman = (res) => {
-    const roman = 
-      ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
-    
-    
-
-
+    for (let i = 1; i < 1000; i++) {
+      romanNumbers.push(generateRomanNumbers(i))
+    }
   }
-  convertToRoman(result) // TODO вставить в if === roman
-  
-  console.log(result.toString());
-  // return result.toString()
+  createRomanNumbers()
+
+  for (let i = 0; i < splittedStr.length; i++) {
+    const romanValue = romanNumbers.indexOf(splittedStr[i])
+    if (romanValue > 0) {
+      splittedStr[i] = romanValue
+    }
+  }
+
+  const resultOperation = Math.floor(eval(splittedStr.join('')));
+
+  try {
+    
+  } catch (error) {
+    
+  }
+
+  console.log(resultOperation);
+
 }
 
-// console.log((`${numbers[0]} ${operator} ${numbers[2]}`));
-
-// const operator = string.split(/\s+/)
-//   .filter((el, i) => {
-//     return allowedOperators.includes(el)
-//   });
-
-// const numbers = string.split(/\s+/)
-//   .filter(el => !operator.includes(el)).map((el) => Number(el));
-
-
-
-
-
-
-// // 'должен работать с десятичными числами (сложение)'
 // calculator('1 + 1') // ('2');
-// calculator('1 + 2') // ('3');
-// calculator('4 + 3') // ('7');
-// calculator('10 + 10') // ('20');
+// calculator('10 - 1') // ('9');
+// calculator('4 - 4') // ('0');
+// calculator('4 - 5') // ('-1');
+// calculator('10 * 10') // ('100');
+// calculator('10 % 1') // ('10');
+// calculator('0 / 4') // ('0');
 
-
-// // 'должен работать с десятичными числами (вычитание)'
-//   calculator('10 - 1') // ('9');
-//   calculator('5 - 4') // ('1');
-//   calculator('4 - 4') // ('0');
-//   calculator('1 - 10') // ('-9');
-//   calculator('4 - 5') // ('-1');
-
-
-
-// // 'должен работать с десятичными числами (умножение)'
-//   calculator('10 * 10') // ('100');
-//   calculator('4 * 10') // ('40');
-//   calculator('5 * 1') // ('5');
-//   calculator('5 * 5') // ('25');
-
-
-
-// // 'должен работать с десятичными числами (деление)'
-//   calculator('10 % 1') // ('10');
-//   calculator('6 / 2') // ('3');
-//   calculator('5 / 4') // ('1');
-//   calculator('0 / 4') // ('0');
-
-
-// 'должен работать с римскими числами (сложение)'
 calculator('I + I') // ('II');
-calculator('I + II') // ('III');
-calculator('IV + III') // ('VII');
-calculator('X + X') // ('XX');
-calculator('X + IX') // ('XIX');
-
-
-// // 'должен работать с римскими числами (вычитание)'
-// calculator('X - I') // ('IX');
-// calculator('V - IV') // ('I');
-// calculator('IV - IV') // ('');
-// calculator('I - X') // ('');
-// calculator('IV - V') // ('');
-
-
-// // 'должен работать с римскими числами (умножение)'
-// calculator('X * X') // ('C');
-// calculator('IV * X') // ('XL');
-// calculator('V * I') // ('V');
-// calculator('V * V') // ('XXV');
-
-
-// // 'должен работать с римскими числами (деление)'
-// calculator('X / I') // ('X');
-// calculator('VI / II') // ('III');
-// calculator('V / IV') // ('I');
-// calculator('II / IV') // ('');
+calculator('X - I') // ('IX');
+calculator('IV - IV') // ('');
+calculator('I - X') // ('');
+calculator('X * X') // ('C');
+calculator('V * I') // ('V');
+calculator('X / I') // ('X');
+calculator('VI / II') // ('III');
+calculator('V / IV') // ('I');
+calculator('II / IV') // ('');
 
 
 // // 'должен выбрасывать ошибку на некорректных данных'
