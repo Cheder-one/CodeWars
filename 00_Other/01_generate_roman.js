2857b0570b3dc5e086fe3438b8a04a1f777fcb20
Cheder-one <cherbadenis1@gmail.com>
2023-01-30 03:38:34 +0300

    u

--- 00_Other/01_generate_roman.js
+++ 00_Other/01_generate_roman.js
@@ -1,6 +1,6 @@
+let romanNum = [''];
 
 function createRomanNumbers () {
-  let romanNum = [''];
 
   const lookup = {
     M:  1000,
@@ -30,8 +30,21 @@ function createRomanNumbers () {
   }
 
   for (let i = 1; i <= 1000; i++) {
-    return romanNum.push(convertToRoman(i));
+    romanNum.push(convertToRoman(i));
   }
 }
 createRomanNumbers ()
-console.log(romanNumerals);
+console.log(romanNum);
+
+//-------------Roman decryption-------------
+function solution(roman){
+  var conversion = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
+  
+  return roman.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((sum, roman) => sum + conversion[roman], 0);
+}
+
+console.log(solution('XXI')) //  21
+console.log(solution('I')) //  1
+console.log(solution('IV')) //  4
+console.log(solution('MMVIII')) //  2008
+console.log(solution('MDCLXVI')) //  1666
