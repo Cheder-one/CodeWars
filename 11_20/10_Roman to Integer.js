fb460296a4fbad605a9442209832ebe0d02765a6
Cheder-one <cherbadenis1@gmail.com>
2023-02-01 22:20:45 +0300

    u

---
+++ 11_20/10_Roman to Integer.js
@@ -0,0 +1,24 @@
+var romanToInt = function (s) {
+   s = s.match(/CM|CD|XC|XL|IX|IV|\w/g);
+   const lookup = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+   };
+   return s.reduce((a, b) => a + lookup[b], 0);
+};
+
+
+console.log(romanToInt("III")); // 3
+console.log(romanToInt("LVIII")); // 58
+console.log(romanToInt("MCMXCIV")); // 1994
