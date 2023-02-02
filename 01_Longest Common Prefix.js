178f65618d8fd83c20d62dfa33293ada4cc931f0
Cheder-one <cherbadenis1@gmail.com>
2023-02-03 02:23:21 +0300

    u

--- 01_Longest Common Prefix.js
+++ 01_Longest Common Prefix.js
@@ -1,31 +1,27 @@
-https://leetcode.com/problems/longest-common-prefix/description/
-https://leetcode.com/problems/greatest-common-divisor-of-strings/
-https://www.youtube.com/watch?v=lJiEoWH_n74
-
 var longestCommonPrefix = function (strs) {
    if (strs.length === 1 && strs[0].length > 0) {
       return strs[0];
-   } 
-   if (strs.length <= 1) { 
-      return ""; 
    }
-   for (const el of strs) {
-      if (el.length < 1) 
+   if (strs.length <= 1) {
       return "";
    }
-
-   for (let i = 0; i < strs.length; i++) {
-      for (let j = 1; j < strs[0].length; j++) {
-         if (strs[0][i]) {
-            
-         }
-      }
+   for (const el of strs) {
+      if (el.length < 1)
+         return "";
    }
 
+   let prefix = '';
+   for (let i = 1; i < strs.length; i++) {
+      for (let j = 0; j < strs[0].length; j++) {
+        
+      }
+      return prefix;
+   }
 };
 
+console.log(longestCommonPrefix(["aaa", "aa", "aaa"])); // aа
 console.log(longestCommonPrefix(["cir", "car"])); // c
-// console.log(longestCommonPrefix(["a"])); // a
-// console.log(longestCommonPrefix(["flower", "flow", "flight"])); // 'fl'
-// console.log(longestCommonPrefix(["dog", "racecar", "car"])); // ''
+console.log(longestCommonPrefix(["a"])); // a
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); // 'fl'
+console.log(longestCommonPrefix(["dog", "racecar", "car"])); // ''
 
