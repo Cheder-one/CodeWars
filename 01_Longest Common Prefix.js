02ffbc12859af0fbb180bb23fd0452061cc25ba2
Cheder-one <cherbadenis1@gmail.com>
2023-02-02 22:46:35 +0300

    u

---
+++ 01_Longest Common Prefix.js
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/longest-common-prefix/description/
+https://leetcode.com/problems/greatest-common-divisor-of-strings/
+var longestCommonPrefix = function (strs) {
+   if (strs.length === 1 && strs[0].length > 0) {
+      return strs[0];
+   } 
+   if (strs.length <= 1) { 
+      return ""; 
+   }
+   for (const el of strs) {
+      if (el.length < 1) 
+      return "";
+   }
+
+   for (let i = 0; i < strs.length; i++) {
+      for (let j = 1; j < strs[0].length; j++) {
+         if (strs[0][i]) {
+            
+         }
+      }
+   }
+
+};
+
+console.log(longestCommonPrefix(["cir", "car"])); // c
+// console.log(longestCommonPrefix(["a"])); // a
+// console.log(longestCommonPrefix(["flower", "flow", "flight"])); // 'fl'
+// console.log(longestCommonPrefix(["dog", "racecar", "car"])); // ''
+
