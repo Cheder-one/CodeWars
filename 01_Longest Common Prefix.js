4aa4e816724ad0b1124841e4a09e799a8b1ff332
Cheder-one <cherbadenis1@gmail.com>
2023-02-02 22:47:11 +0300

    u

--- 01_Longest Common Prefix.js
+++ 01_Longest Common Prefix.js
@@ -1,5 +1,7 @@
 https://leetcode.com/problems/longest-common-prefix/description/
 https://leetcode.com/problems/greatest-common-divisor-of-strings/
+https://www.youtube.com/watch?v=lJiEoWH_n74
+
 var longestCommonPrefix = function (strs) {
    if (strs.length === 1 && strs[0].length > 0) {
       return strs[0];
