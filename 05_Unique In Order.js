2506dd67b035870104508193324a371447e66f38
Cheder-one <cherbadenis1@gmail.com>
2023-02-04 18:36:44 +0300

    u

--- 05_Unique In Order.js
+++ 05_Unique In Order.js
@@ -31,15 +31,30 @@ var uniqueInOrder = function (iterable) {
    } catch { }
 
    return iterable.reduce((sum, n) => {
-      
-   })
+      if (sum[sum.length - 1] !== n) {
+         sum.push(n)
+      }
+      return sum
+   }, [])
 };
 
+//-------------Analog-------------
+function uniqueInOrder(iterable) {
+   var result = []
+   var last
+
+   for (var i = 0; i < iterable.length; i++) {
+      if (iterable[i] !== last) {
+         result.push(last = iterable[i])
+      }
+   // Or
+      // if (iterable[i] !== result[result.length-1]) {
+      //    result.push(iterable[i])
+      // }
+   }
+   return result
+}
+
 console.log(uniqueInOrder('AAAABBBCCDAABBB')) // ['A', 'B', 'C', 'D', 'A', 'B']
-// console.log(uniqueInOrder('ABBCcAD')) // ['A', 'B', 'C', 'c', 'A', 'D']
-// console.log(uniqueInOrder([1, 2, 2, 3, 3])) // [1,2,3]
-
-// const x = [1, 2, 3]
-
-// const res = x.join('')
-// console.log(res);
+console.log(uniqueInOrder('ABBCcAD')) // ['A', 'B', 'C', 'c', 'A', 'D']
+console.log(uniqueInOrder([1, 2, 2, 3, 3])) // [1,2,3]
