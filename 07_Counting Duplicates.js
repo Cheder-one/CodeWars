083e798c7127cd6770165391fccaf751a27d0331
Cheder-one <cherbadenis1@gmail.com>
2023-02-06 17:37:58 +0300

    u

--- 07_Counting Duplicates.js
+++ 07_Counting Duplicates.js
@@ -1,12 +1,10 @@
-https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/javascript
 function duplicateCount(text) {
    text = text.toLowerCase().split('');
    let arr = [];
 
    for (let i = 0; i < text.length; i++) {
-      let el = text[i];
-      if (arr.indexOf(el) === -1) {
-         arr.push(el);
+      if (!arr.includes(text[i])) {
+         arr.push(text[i]);
       }
    }
    return arr
