c40eacc748ca295fcc8a498aae41bd8a66d5f91c
Cheder-one <cherbadenis1@gmail.com>
2023-02-09 22:52:04 +0300

    u

--- 21_30/09_Valid Parentheses.js
+++ 21_30/09_Valid Parentheses.js
@@ -1,26 +1,26 @@
 var isValid = function (s) {
-   const patterns = ['()', '[]', '{}'];
+   const patterns = ["()", "[]", "{}"];
 
    while (s.length % 2 === 0) {
-      const before = s.length
+      const before = s.length;
       for (const el of patterns) {
-         s = s.split(el).join('');
+         s = s.split(el).join("");
       }
       const after = s.length;
 
       if (s.length === 0) {
-         return true
+         return true;
       } else if (before === after) {
-         return false
+         return false;
       }
    }
-   return false
+   return false;
 };
 
 //-------------Refactoring-------------
 var isValid = function (s) {
    const stack = [];
-   const mapping = {
+   const map = {
       "(": ")",
       "[": "]",
       "{": "}",
@@ -28,21 +28,26 @@ var isValid = function (s) {
 
    for (let i = 0; i < s.length; i++) {
       const el = s[i];
-      
-
+      if (el in map) {
+         stack.push(el);
+      } else {
+         // Получается, если мы не нашли `el` в ключах объекта, значит это закрывающая скобка. А она должна быть валидна последней открывающей.
+         const lastOpenBrkt = stack.pop();
+         map[lastOpenBrkt];
+         if (el !== map[lastOpenBrkt]) {
+            return false;
+         }
+      }
    }
-
+   return stack.length === 0;
 };
 
+isValid("()"); // true
+isValid("()[]{}"); // true
+isValid("[()]{}"); // true
+isValid("[[()[]]]"); // true
 
-// isValid("()"); // true
-// isValid("()[]{}"); // true
-// isValid("[()]{}"); // true
-// isValid("[[()[]]]"); // true
-
-// isValid("(]"); // false
-// isValid("[{]"); // false
-// isValid("([)]"); // false
-// isValid("[[()[]]]({}[[][]{{}})]"); // false
-
-
+isValid("(]"); // false
+isValid("[{]"); // false
+isValid("([)]"); // false
+isValid("[[()[]]]({}[[][]{{}})]"); // false
