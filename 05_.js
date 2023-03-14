1774974c82334dc9619b03f4992eb644499b9208
Cheder-one <cherbadenis1@gmail.com>
2023-03-15 00:15:24 +0300

    u

--- 05_.js
+++ 05_.js
@@ -1,64 +1,64 @@
-function decodeMorse(code) {
-  const MORSE_CODE = {
-    ".-": "A",
-    "-...": "B",
-    "-.-.": "C",
-    "-..": "D",
-    ".": "E",
-    "..-.": "F",
-    "--.": "G",
-    "....": "H",
-    "..": "I",
-    ".---": "J",
-    "-.-": "K",
-    ".-..": "L",
-    "--": "M",
-    "-.": "N",
-    "---": "O",
-    ".--.": "P",
-    "--.-": "Q",
-    ".-.": "R",
-    "...": "S",
-    "-": "T",
-    "..-": "U",
-    "...-": "V",
-    ".--": "W",
-    "-..-": "X",
-    "-.--": "Y",
-    "--..": "Z",
-    "-----": "0",
-    ".----": "1",
-    "..---": "2",
-    "...--": "3",
-    "....-": "4",
-    ".....": "5",
-    "-....": "6",
-    "--...": "7",
-    "---..": "8",
-    "----.": "9",
-    ".-.-.-": ".",
-    "--..--": ",",
-    "..--..": "?",
-    "-.-.--": "!",
-    "-....-": "-",
-    "-..-.": "/",
-    ".--.-.": "@",
-    "-.--.": "(",
-    "-.--.-": ")",
-    "-.-.--": "!",
-    "-...-": "=",
-    ".-.-.-": ".",
-    "--..--": ",",
-    "..--..": "?",
-    "...---...": "SOS",
-    "": " ",
-  };
+decodeMorse = function (morseCode) {
+  const morse = new Map();
+  morse.set(".-", "A");
+  morse.set("-...", "B");
+  morse.set("-.-.", "C");
+  morse.set("-..", "D");
+  morse.set(".", "E");
+  morse.set("..-.", "F");
+  morse.set("--.", "G");
+  morse.set("....", "H");
+  morse.set("..", "I");
+  morse.set(".---", "J");
+  morse.set("-.-", "K");
+  morse.set(".-..", "L");
+  morse.set("--", "M");
+  morse.set("-.", "N");
+  morse.set("---", "O");
+  morse.set(".--.", "P");
+  morse.set("--.-", "Q");
+  morse.set(".-.", "R");
+  morse.set("...", "S");
+  morse.set("-", "T");
+  morse.set("..-", "U");
+  morse.set("...-", "V");
+  morse.set(".--", "W");
+  morse.set("-..-", "X");
+  morse.set("-.--", "Y");
+  morse.set("--..", "Z");
+  morse.set("-----", "0");
+  morse.set(".----", "1");
+  morse.set("..---", "2");
+  morse.set("...--", "3");
+  morse.set("....-", "4");
+  morse.set(".....", "5");
+  morse.set("-....", "6");
+  morse.set("--...", "7");
+  morse.set("---..", "8");
+  morse.set("----.", "9");
+  morse.set(".-.-.-", ".");
+  morse.set("--..--", ",");
+  morse.set("..--..", "?");
+  morse.set("-.-.--", "!");
+  morse.set("-....-", "-");
+  morse.set("-..-.", "/");
+  morse.set(".--.-.", "@");
+  morse.set("-.--.", "(");
+  morse.set("-.--.-", ")");
+  morse.set("-...-", "=");
+  morse.set("...---...", "SOS");
+  morse.set("", " ");
 
-  const words = code.trim().split("   ");
-  const decodedWords = words.map((word) => {
-    const chars = word.split(" ");
-    const decodedChars = chars.map((char) => MORSE_CODE[char]);
-    return decodedChars.join("");
-  });
-  return decodedWords.join(" ");
-}
+  morseCode = morseCode.split("   ").map((el) => el.split(" "));
+  let result = [];
+
+  for (const word of morseCode) {
+    const dcrptWord = word.map((letter) => {
+      return morse.get(letter);
+    });
+    result = [...result, ...dcrptWord, " "];
+  }
+  return result.join("").trim();
+};
+
+console.log(decodeMorse(".... . -.--   .--- ..- -.. .")); // 'HEY JUDE'
