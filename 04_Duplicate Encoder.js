bc50d52b5fe623bc06d5ec9f4af8588de883e740
Cheder-one <cherbadenis1@gmail.com>
2023-03-14 21:45:29 +0300

    u

--- 04_Duplicate Encoder.js
+++ 04_Duplicate Encoder.js
@@ -1,10 +1,31 @@
 function duplicateEncode(word) {
   word = word.toLowerCase();
-  const map = new Map();
-  console.log(map);
+  const chars = new Map();
+
+  for (let elem of word) {
+    if (chars.has(elem)) {
+      chars.set(elem, chars.get(elem) + 1);
+    } else {
+      chars.set(elem, 1);
+    }
+  }
+
+  return word
+    .split("")
+    .map((el) => {
+      return chars.get(el) > 1 ? ")" : "(";
+    })
+    .join("");
 }
 
-duplicateEncode("Bin"); // "((("
+// duplicateEncode("Dann"); // "(())"
 // duplicateEncode("recede"); // "()()()"
 // duplicateEncode("Success"); // ")())())" should ignore case
-// duplicateEncode("(( @"); // "))(("
+console.log(duplicateEncode("(( @")); // "))(("
+
+// const arr = [1, 2, 3, 2, 1, 3, 2, 1];
+// const counts = new Map();
+
+// counts.set(arr[1], 1);
+// console.log(counts);
+// console.log(counts.get(arr[1]));
