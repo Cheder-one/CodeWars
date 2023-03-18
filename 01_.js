c66ab2b85f137e9360615e70d5694de04a7483b0
Cheder-one <cherbadenis1@gmail.com>
2023-03-18 17:39:24 +0300

    u

---
+++ 01_.js
@@ -0,0 +1,11 @@
+var climbStairs = function (n) {
+  for (let i = 0; n > 0; i++) {
+    return 2;
+  }
+};
+
+console.assert(climbStairs(2) === 2);
+console.assert(climbStairs(3) === 3);
+console.assert(climbStairs(4) === 5);
+console.assert(climbStairs(5) === 8);
+console.assert(climbStairs(6) === 13);
