49c0d0a435289da2d0d4164a94be18ffaa0f9958
Cheder-one <cherbadenis1@gmail.com>
2023-04-01 22:55:57 +0300

    u

--- 04_.js
+++ 04_.js
@@ -1,5 +1,32 @@
-function solution(str) {}
+function solution(str) {
+  const strLen = str.split("").length;
+  if (strLen === 0) return [];
 
-solution("abcdef"); // ["ab", "cd", "ef"];
+  const isEval = strLen % 2 === 0;
+  if (!isEval) str += "_";
+  let splittedStr = str.split("");
+
+  for (let i = 2; i < splittedStr.length; i += 2) {
+    splittedStr[i] = "|" + splittedStr[i];
+  }
+  return splittedStr.join("").split("|");
+}
+
+//-------------Refactoring-------------
+function solution(str) {
+  if (str.length % 2 !== 0) str += "_";
+  let res = [];
+  for (let i = 2; i <= str.length; i += 2) {
+    res.push(str[i - 2] + str[i - 1]);
+  }
+  return res;
+}
+
+//-------------OR-------------
+function solution(str) {
+  return (str.length % 2 ? str + "_" : str).match(/../g);
+}
+
+// solution("abcdef"); // ["ab", "cd", "ef"];
 solution("abcdefg"); // ["ab", "cd", "ef", "g_"];
-solution(""); // [];
+// solution(""); // [];
